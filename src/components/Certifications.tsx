96076e3d1d2ea645100c4b35d9e7d12f91381023
Aditya Mane <140746634+Aditya-Mane0411@users.noreply.github.com>
2025-11-01 15:38:02 +0530

    Update certifications with new titles and dates

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -55,20 +55,36 @@ const CertificationCard = ({ title, issuer, date, credentialId, imageUrl, link }
 
 const Certifications = () => {
   const certifications = [
-    {
-      title: "JavaScript Essentials ",
+        {
+      title: "DSA in Python",
+      issuer: "Udemy",
+      date: "March 2025",
+   //   credentialId: "SCRUM87654321",
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMTp5_rdoavOMP26C_CmQO_NqSzMw-qA-OVQ&s",
+      link: "https://www.playbook.com/s/adityamane/pNDjYgriZFdTuH8byctBKqzq?assetToken=n8jeYoQ5UDvtqQ6YjwLkfq17"
+    },
+     {
+      title: "Google Cybersecurity",
+      issuer: "Coursera",
+      date: "June 2024",
+    //  credentialId: "NODE98765432",
+      imageUrl: "https://images.credly.com/images/0bf0f2da-a699-4c82-82e2-56dcf1f2e1c7/image.png",
+      link: "https://www.playbook.com/s/adityamane/KKST8gt6xWAieTH1Da1tv8gj?assetToken=okuVtfpAWkDex88sDRNBTM7u"
+    },
+     {
+      title: "Cyber Threat Management",
       issuer: "Cisco",
-      date: "July 2023",
-      //credentialId: "AWS123456789",
-      imageUrl: "https://www.infoworld.com/wp-content/uploads/2025/05/2263137-0-24139200-1747637392-shutterstock_1361674454-100939444-orig.jpg?quality=50&strip=all",
-      link: "https://www.playbook.com/s/adityamane/4ULpMiTZvThDSGC8YP4G6ry6?assetToken=3Lo14krewBXkxTknPfWNShHv"
+      date: "April 2024",
+    //  credentialId: "META12345678",
+      imageUrl: "https://images.unsplash.com/photo-1558494949-ef010cbdcc31",
+      link: "https://www.playbook.com/s/adityamane/n9FnsCozJ5zodGsQfuzjFQMJ?assetToken=LRRscicTb7H3TCpaUkmCvRvX"
     },
     {
-      title: "Python Essentials",
-      issuer: "Cisco",
-      date: "July 2023",
-      //credentialId: "MS987654321",
-      imageUrl: "https://cdn.activestate.com/wp-content/uploads/2021/12/python-coding-mistakes.jpg",
+      title: "Google AI Essentials",
+      issuer: "Coursera",
+      date: "June 2024",
+    //  credentialId: "GCP234567891",
+      imageUrl: "https://www.computerworld.com/wp-content/uploads/2025/02/2517498-0-09119900-1740378700-shutterstock_offset_2321455729.jpg?quality=50&strip=all&w=1024",
       link: "https://www.playbook.com/s/adityamane/Wyyysz1deNgtbFygm7igU9kL?assetToken=4E4Bz7HkSeffJUJrdVPXuvgx"
     },
     {      
@@ -79,37 +95,21 @@ const Certifications = () => {
       imageUrl: "https://amigoscode.com/assets/thumbnails/courses/java-for-developers.webp",
       link: "https://www.scrum.org/certificates"
     },
-    {
-      title: "Google AI Essentials",
-      issuer: "Coursera",
-      date: "June 2024",
-    //  credentialId: "GCP234567891",
-      imageUrl: "https://www.computerworld.com/wp-content/uploads/2025/02/2517498-0-09119900-1740378700-shutterstock_offset_2321455729.jpg?quality=50&strip=all&w=1024",
+      {
+      title: "Python Essentials",
+      issuer: "Cisco",
+      date: "July 2023",
+      //credentialId: "MS987654321",
+      imageUrl: "https://cdn.activestate.com/wp-content/uploads/2021/12/python-coding-mistakes.jpg",
       link: "https://www.playbook.com/s/adityamane/Wyyysz1deNgtbFygm7igU9kL?assetToken=4E4Bz7HkSeffJUJrdVPXuvgx"
     },
     {
-      title: "Cyber Threat Management",
+      title: "JavaScript Essentials ",
       issuer: "Cisco",
-      date: "April 2024",
-    //  credentialId: "META12345678",
-      imageUrl: "https://images.unsplash.com/photo-1558494949-ef010cbdcc31",
-      link: "https://www.playbook.com/s/adityamane/n9FnsCozJ5zodGsQfuzjFQMJ?assetToken=LRRscicTb7H3TCpaUkmCvRvX"
-    },
-    {
-      title: "Google Cybersecurity",
-      issuer: "Coursera",
-      date: "June 2024",
-    //  credentialId: "NODE98765432",
-      imageUrl: "https://images.credly.com/images/0bf0f2da-a699-4c82-82e2-56dcf1f2e1c7/image.png",
-      link: "https://www.playbook.com/s/adityamane/KKST8gt6xWAieTH1Da1tv8gj?assetToken=okuVtfpAWkDex88sDRNBTM7u"
-    },
-    {
-      title: "DSA in Python",
-      issuer: "Udemy",
-      date: "March 2025",
-   //   credentialId: "SCRUM87654321",
-      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMTp5_rdoavOMP26C_CmQO_NqSzMw-qA-OVQ&s",
-      link: "https://www.playbook.com/s/adityamane/KKST8gt6xWAieTH1Da1tv8gj?assetToken=okuVtfpAWkDex88sDRNBTM7u"
+      date: "July 2023",
+      //credentialId: "AWS123456789",
+      imageUrl: "https://www.infoworld.com/wp-content/uploads/2025/05/2263137-0-24139200-1747637392-shutterstock_1361674454-100939444-orig.jpg?quality=50&strip=all",
+      link: "https://www.playbook.com/s/adityamane/4ULpMiTZvThDSGC8YP4G6ry6?assetToken=3Lo14krewBXkxTknPfWNShHv"
     }
   ];
 
